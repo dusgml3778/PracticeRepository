c0b46ffb287345c8864ba0a25c6b954480c6216d
kimmingye <dusgml3779@gmail.com>
2021-10-22 22:40:20 +0900

    Js Array

--- Array.js
+++ Array.js
@@ -1,25 +1,52 @@
-const fruits = ['Apple','Banana']
+const fruits = ['Apple', 'Banana']
 
-const a = fruits.forEach(function(fruit,index){
+const a = fruits.forEach(function (fruit, index) {
 
   console.log(`${fruit}-${index}`)
 
 })
 console.log(a)
 
-const b = fruits.map(function(fruit,index){
+const b = fruits.map(function (fruit, index) {
   return `${fruit}-${index}`
 })
 
 console.log(b)
 
-
-const c = fruits.map(function(fruit,index){
+const c = fruits.map(function (fruit, index) {
   return {
 
-    id:index,
-    name:fruit
+    id: index,
+    name: fruit
   }
 })
 
 console.log(c)
+
+const numbers = [1, 2, 3, 4]
+
+// 返却して新しい配列として返却
+const d = numbers.map(num => num < 3)
+
+console.log(d)
+
+// 条件に合うデータを探す
+const e = numbers.filter(num => num < 3)
+
+console.log(e)
+
+// 文字を検索
+const f = fruits.find(f =>
+
+  /^B/.test(f)
+)
+
+console.log(f)
+
+//Index 返却
+const h = fruits.findIndex(f => {
+
+  return /^B/.test(f)
+})
+
+console.log(h)
