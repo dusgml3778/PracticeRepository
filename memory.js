e8c4ca042cf5a69e7259a9083c7398d6c77d2892
kimmingye <dusgml3779@gmail.com>
2021-10-27 00:17:21 +0900

    import & export of JS

--- memory.js
+++ memory.js
@@ -1,23 +1,35 @@
-import _ from 'lodash'
+// import _ from 'lodash'
+import getType from './getType'
+// import {
+//   random,
+//   user
+// } from './getRandom'
 
-// Shallow copy,Deep copy
-const user = {
+import * as R from './getRandom'
 
-  name: 'Heropy',
-  age: 85,
-  emails: ['dusgml3779@gmail.com']
+// // Shallow copy,Deep copy
+// const user = {
 
-}
+//   name: 'Heropy',
+//   age: 85,
+//   emails: ['dusgml3779@gmail.com']
 
-// const copyUser = Object.assign({},user);
-const copyUser = _.cloneDeep(user)
+// }
 
-console.log(copyUser === user)
+// // const copyUser = Object.assign({},user);
+// const copyUser = _.cloneDeep(user)
 
-user.age = 22
-user.emails.push('baeruni1122@gmail.com')
+// console.log(copyUser === user)
 
-console.log(user)
-console.log(copyUser)
+// user.age = 22
+// user.emails.push('baeruni1122@gmail.com')
 
-console.log(user.emails === copyUser.emails)
+// console.log(user)
+// console.log(copyUser)
+
+// console.log(user.emails === copyUser.emails)
+
+const str = getType('aaaa')
+
+console.log(str);
+console.log(R.random())
